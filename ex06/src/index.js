7e97b0ba0273fd6cbc6c77f09a6fb807a0ff3548
Adnan Hamzagic <adnan.hamzagic11@gmail.com>
2021-03-17 17:39:09 +0100

    Created ex06

---
+++ ex06/src/index.js
@@ -0,0 +1,15 @@
+// Only change code below this line
+
+// Only change code above this line
+var lion ={
+    name:"Simba",
+    legs:4,
+    tails:1
+}
+function myFunction(roar,roar){
+    lion.roar="roar-roar";
+
+    return lion;
+}
+console.log(myFunction("roar-roar",lion));
+module.exports=myFunction;
